dd1a370ec12759ea81f86e6b19296e1178ec5377
cra0zy <cra0zy@gmail.com>
2017-08-12 00:01:41 +0200

    Fix stuff

--- generate.sh
+++ generate.sh
@@ -9,21 +9,20 @@ then
 	echo "Downloading Xamarin.Android from:"
 	echo "https://jenkins.mono-project.com/view/Xamarin.Android/job/xamarin-android-linux/lastSuccessfulBuild/Azure/"
 	echo "and place the file in the current directory with the name 'XAL.zip'"
+	cd ..
 	exit
 fi
 
 echo "Extracting Xamarin.Android..."
 unzip ../XAL.zip -d XAL
 
-exit
-
 echo "Copying installer data..."
 cp -rf XAL/*/bin/Release/ mono-android/
 cp -rf ../src/. .
 
-rm XAL.zip
 rm -rf XAL
 cd ..
+rm XAL.zip
 
 echo "Building the installer..."
 rm -rf bin
